c9241681bcb7807cc973219598569542aeef8614
agrawal-vivek <vivek.agrawal31121995@gmail.com>
2023-03-29 22:31:15 +0530

    structure

--- frontend/src/pages/Allroutes.jsx
+++ frontend/src/pages/Allroutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes} from "react-router-dom";
 import Products from './Products/Products';
 const Allroutes = () => {
+  
   return (
     <Routes >
         <Route path='/' element={<h1>Home page</h1>}/>
