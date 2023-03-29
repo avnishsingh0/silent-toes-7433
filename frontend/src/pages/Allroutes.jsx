d4b9910e172ca67b5f1897e90fd6393da3e46073
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-03-29 22:00:45 +0530

    LandingPage and footer

--- frontend/src/pages/Allroutes.jsx
+++ frontend/src/pages/Allroutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, Routes} from "react-router-dom";
+import Home from './HomePage/HomePage';
 import Products from './Products/Products';
 const Allroutes = () => {
   return (
     <Routes >
-        <Route path='/' element={<h1>Home page</h1>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/product' element={<Products/>}/>
     </Routes>
   )
