17ab6eb8337757af57f7b7cff2b538c6024876cc
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-03-30 22:42:19 +0530

    Admin-Customers

--- frontend/src/pages/Allroutes.jsx
+++ frontend/src/pages/Allroutes.jsx
@@ -8,7 +8,7 @@ import Orders from "./Order/Orders";
 import OrderHistory from "./OrderHistory/OrderHistory";
 import ProductDetails from "./Products/ProductDetails";
 import Multistep from "./Admin/Package";
-
+import Customers from "./Admin/Customers";
 import Products from "./Products/Products";
 import Blog from "./Admin/Dashboard";
 const Allroutes = () => {
@@ -19,7 +19,7 @@ const Allroutes = () => {
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/adminpanel" element={<Blog />} />
       <Route path="/adminpanel/package" element={<Multistep />} />
-      {/* <Route path="/adminpanel/customers" element={<Customers />} /> */}
+      <Route path="/adminpanel/customers" element={<Customers />} />
       {/* <Route path="/adminpanel/order" element={<Order />} /> */}
       <Route path="/payment" element={<Payment />} />
       <Route path="/orders" element={<Orders />} />
