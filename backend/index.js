3627f7af2a87d3af1120c2bcca9029b85457b65e
avnish <avnishbdp@gmail.com>
2023-03-31 14:21:40 +0530

    product_api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express= require("express")
 const {connection}= require("./configs/db")
+const { productRouter } = require("./routes/product.routes")
 const {userRouter}=require('./routes/user.routes')
+const cors = require("cors");
 const app= express()
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("Welcome Home Page");
+});
+
 app.use('/users',userRouter)
+app.use("/product", productRouter);
 
 app.listen(4500,async()=>{
     try {
